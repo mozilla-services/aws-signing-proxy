31b997a0806464a950fdc1bafd3795e1de835c2c
Miles Crabill <mcrabill@mozilla.com>
2018-02-14 11:57:38 -0800

    address golint, gofmt nits

--- main.go
+++ main.go
@@ -1,165 +1,165 @@
 package main
 
 import (
-    "github.com/aws/aws-sdk-go/aws"
-    "github.com/aws/aws-sdk-go/aws/ec2metadata"
-    "github.com/aws/aws-sdk-go/aws/session"
-    "github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/aws/signer/v4"
 
-    "github.com/sha1sum/aws_signing_client"
-    "github.com/kelseyhightower/envconfig"
-    "github.com/DataDog/datadog-go/statsd"
+	"github.com/DataDog/datadog-go/statsd"
+	"github.com/kelseyhightower/envconfig"
+	"github.com/sha1sum/aws_signing_client"
 
-    "crypto/x509"
-    "crypto/tls"
-    "fmt"
-    "io/ioutil"
-    "net/url"
-    "net/http"
-    "time"
+	"crypto/tls"
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"time"
 
-    "github.com/milescrabill/aws-signing-proxy/proxy"
+	"github.com/milescrabill/aws-signing-proxy/proxy"
 )
 
 const (
-    appNamespace = "SIGNING_PROXY"
+	appNamespace = "SIGNING_PROXY"
 )
 
 var (
-    statsdClient *statsd.Client
-    httpClient *http.Client
-    pool *x509.CertPool
+	statsdClient *statsd.Client
+	httpClient   *http.Client
+	pool         *x509.CertPool
 )
 
 // get CA certs for our http.Client
 func init() {
-    // cacert.pem is a runtime dependency!
-    bs, err := ioutil.ReadFile("cacert.pem")
-    if err != nil {
-        panic(err)
-    }
+	// cacert.pem is a runtime dependency!
+	bs, err := ioutil.ReadFile("cacert.pem")
+	if err != nil {
+		panic(err)
+	}
 
-    pool = x509.NewCertPool()
-    pool.AppendCertsFromPEM(bs)
+	pool = x509.NewCertPool()
+	pool.AppendCertsFromPEM(bs)
 
-    // default http client with a timeout
-    httpClient = &http.Client{
-        Timeout: time.Second * 10,
-        Transport: &http.Transport{
-            TLSClientConfig: &tls.Config{RootCAs: pool},
-        },
-    }
+	// default http client with a timeout
+	httpClient = &http.Client{
+		Timeout: time.Second * 10,
+		Transport: &http.Transport{
+			TLSClientConfig: &tls.Config{RootCAs: pool},
+		},
+	}
 }
 
-func LoggingMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        fmt.Println(r.RequestURI)
-        next.ServeHTTP(w, r)
-    })
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.RequestURI)
+		next.ServeHTTP(w, r)
+	})
 }
 
-func StatsdMiddleware(next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        statsdClient.Incr("requests", []string{}, 1.0)
-        next.ServeHTTP(w, r)
-    })
+func statsdMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		statsdClient.Incr("requests", []string{}, 1.0)
+		next.ServeHTTP(w, r)
+	})
 }
 
 func getEC2Tags(metadata *ec2metadata.EC2Metadata) []string {
-    region, err := metadata.Region()
-    if err != nil {
-        panic(err)
-    }
-    return []string{
-        "region:" + region,
-    }
+	region, err := metadata.Region()
+	if err != nil {
+		panic(err)
+	}
+	return []string{
+		"region:" + region,
+	}
 }
 
 func main() {
-    config := struct {
-        // SIGNING_PROXY_LOG_REQUESTS
-        LogRequests bool `default:"true" split_words:"true"`
-        Statsd bool `default:"true"`
-        // SIGNING_PROXY_STATSD_LISTEN
-        StatsdListen string `default:"127.0.0.1:8125" split_words:"true"`
-        Listen string `default:"localhost:8000"`
-        Service string `default:"s3"`
-        Region string `default:"us-east-1"`
-        Destination string `default:"https://s3.amazonaws.com"`
-    }{}
+	config := struct {
+		// SIGNING_PROXY_LOG_REQUESTS
+		LogRequests bool `default:"true" split_words:"true"`
+		Statsd      bool `default:"true"`
+		// SIGNING_PROXY_STATSD_LISTEN
+		StatsdListen string `default:"127.0.0.1:8125" split_words:"true"`
+		Listen       string `default:"localhost:8000"`
+		Service      string `default:"s3"`
+		Region       string `default:"us-east-1"`
+		Destination  string `default:"https://s3.amazonaws.com"`
+	}{}
 
-    // load envconfig
-    err := envconfig.Process(appNamespace, &config)
-    if err != nil {
-        panic(err)
-    }
+	// load envconfig
+	err := envconfig.Process(appNamespace, &config)
+	if err != nil {
+		panic(err)
+	}
 
-    // *url.URL for convenience
-    destinationURL, err := url.Parse(config.Destination)
-    if err != nil {
-        panic(err)
-    }
+	// *url.URL for convenience
+	destinationURL, err := url.Parse(config.Destination)
+	if err != nil {
+		panic(err)
+	}
 
-    // initialize AWS session
-    sess := session.Must(session.NewSession(&aws.Config{
-        Region: aws.String(config.Region),
-    }))
+	// initialize AWS session
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(config.Region),
+	}))
 
-    ec2tags := []string{}
-    metadata := ec2metadata.New(sess)
-    if metadata.Available() {
-        ec2tags = getEC2Tags(metadata)
-    }
+	ec2tags := []string{}
+	metadata := ec2metadata.New(sess)
+	if metadata.Available() {
+		ec2tags = getEC2Tags(metadata)
+	}
 
-    // create signing http client
-    signer := v4.NewSigner(sess.Config.Credentials)
-    signingClient, err := aws_signing_client.New(
-        signer,
-        httpClient,
-        config.Service,
-        config.Region,
-    )
-    if err != nil {
-        panic(err)
-    }
+	// create signing http client
+	signer := v4.NewSigner(sess.Config.Credentials)
+	signingClient, err := aws_signing_client.New(
+		signer,
+		httpClient,
+		config.Service,
+		config.Region,
+	)
+	if err != nil {
+		panic(err)
+	}
 
-    // create proxy using signing http client
-    prxy, err := proxy.New(
-        destinationURL,
-        signingClient,
-    )
-    if err != nil {
-        panic(err)
-    }
+	// create proxy using signing http client
+	prxy, err := proxy.New(
+		destinationURL,
+		signingClient,
+	)
+	if err != nil {
+		panic(err)
+	}
 
-    var handler http.Handler
-    handler = prxy
+	var handler http.Handler
+	handler = prxy
 
-    // wrap handler for logging
-    if config.LogRequests {
-        handler = LoggingMiddleware(handler)
-    }
+	// wrap handler for logging
+	if config.LogRequests {
+		handler = loggingMiddleware(handler)
+	}
 
-    // wrap handler for statsd
-    if config.Statsd {
-        statsdClient, err := statsd.New(config.StatsdListen)
-        if err != nil {
-            panic(err)
-        }
-        // prepends metrics
-        statsdClient.Namespace = appNamespace + "."
-        statsdClient.Tags = append(statsdClient.Tags, ec2tags...)
-        handler = StatsdMiddleware(handler)
-    }
+	// wrap handler for statsd
+	if config.Statsd {
+		statsdClient, err := statsd.New(config.StatsdListen)
+		if err != nil {
+			panic(err)
+		}
+		// prepends metrics
+		statsdClient.Namespace = appNamespace + "."
+		statsdClient.Tags = append(statsdClient.Tags, ec2tags...)
+		handler = statsdMiddleware(handler)
+	}
 
-    // sane default timeouts
-    srv := &http.Server{
-        Addr: config.Listen,
-        ReadTimeout: 5 * time.Second,
-        WriteTimeout: 10 * time.Second,
-        IdleTimeout: 60 * time.Second,
-        Handler: handler,
-    }
+	// sane default timeouts
+	srv := &http.Server{
+		Addr:         config.Listen,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  60 * time.Second,
+		Handler:      handler,
+	}
 
-    fmt.Println(srv.ListenAndServe())
+	fmt.Println(srv.ListenAndServe())
 }
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,70 +1,72 @@
 package proxy
 
 import (
-    "fmt"
-    "io"
-    "net/url"
-    "net/http"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
 )
 
 var (
-    // error types
-    MissingDestinationError = fmt.Errorf("No destination specified.")
+	// ErrMissingDestination is returned when New is called with a nil destination
+	ErrMissingDestination = fmt.Errorf("no destination specified")
 )
 
-func New(destination *url.URL, client *http.Client) (*ProxyHandler, error) {
-    if destination == nil {
-        return nil, MissingDestinationError
-    }
-    if client == nil {
-        client = http.DefaultClient
-    }
-    return &ProxyHandler{destination, client}, nil
+// New creates a Handler using the input destination and client
+func New(destination *url.URL, client *http.Client) (*Handler, error) {
+	if destination == nil {
+		return nil, ErrMissingDestination
+	}
+	if client == nil {
+		client = http.DefaultClient
+	}
+	return &Handler{destination, client}, nil
 }
 
-type ProxyHandler struct {
-    destination *url.URL
-    client *http.Client
+// Handler satisfies http.Handler
+type Handler struct {
+	destination *url.URL
+	client      *http.Client
 }
 
-func (proxy ProxyHandler) newProxiedRequest(r *http.Request) (*http.Request, error) {
-    proxiedURL := *r.URL
-    proxiedURL.Host = proxy.destination.Host
-    proxiedURL.Scheme = proxy.destination.Scheme
+func (proxy Handler) newProxiedRequest(r *http.Request) (*http.Request, error) {
+	proxiedURL := *r.URL
+	proxiedURL.Host = proxy.destination.Host
+	proxiedURL.Scheme = proxy.destination.Scheme
 
-    return http.NewRequest(
-        r.Method,
-        proxiedURL.String(),
-        r.Body,
-    )
+	return http.NewRequest(
+		r.Method,
+		proxiedURL.String(),
+		r.Body,
+	)
 }
 
 // satisfies http.Handler
-func (proxy ProxyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    defer r.Body.Close()
+func (proxy Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
 
-    proxiedReq, err := proxy.newProxiedRequest(r)
-    if err != nil {
-        http.Error(w, "Internal Server Error", 500)
-        return
-    }
+	proxiedReq, err := proxy.newProxiedRequest(r)
+	if err != nil {
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
 
-    resp, err := proxy.client.Do(proxiedReq)
-    if err != nil {
-        panic(err)
-    }
-    defer resp.Body.Close()
+	resp, err := proxy.client.Do(proxiedReq)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
 
-    // add all response headers to request
-    for k, vs := range resp.Header {
-        for _, v := range vs {
-            w.Header().Add(k, v)
-        }
-    }
+	// add all response headers to request
+	for k, vs := range resp.Header {
+		for _, v := range vs {
+			w.Header().Add(k, v)
+		}
+	}
 
-    w.WriteHeader(resp.StatusCode)
-    _, err = io.Copy(w, resp.Body)
-    if err != nil {
-        panic(err)
-    }
+	w.WriteHeader(resp.StatusCode)
+	_, err = io.Copy(w, resp.Body)
+	if err != nil {
+		panic(err)
+	}
 }
